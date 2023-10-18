6a66df64af6b8ef6abad27dd079aedfd0a12b363
Robin Bate Boerop <me@robinbb.com>
2023-10-18 12:24:45 -0700

    Update Gandi.net config

--- gandi-nixos-kvm/configuration.nix
+++ gandi-nixos-kvm/configuration.nix
@@ -4,42 +4,68 @@
     [ <nixpkgs/nixos/modules/virtualisation/openstack-config.nix>
     ];
 
-  services.buildkite-agents."gandi-nixos" = {
-    enable = true;
-    name = "gandi-nixos";
-    tokenPath = "/run/keys/buildkite-token";  # Requires manual install!
-    extraConfig = "spawn=6";
-    tags = {
-      kvm = "true";
-      podman = "false";
-    };
+  users.users.buildkite = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    extraGroups = [ "wheel" ];
+    autoSubUidGidRange = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBgTr9+Krb77UzfMIIo0iKEfClzlKl82WZupmY6lTVp"
+    ];
+  };
+
+  systemd.user.services.buildkite.wantedBy = ["multi-user.target"];
+  systemd.user.services.buildkite.after = ["network.target"];
+  systemd.user.services.buildkite.serviceConfig = {
+    ExecStart = "${pkgs.buildkite-agent}/bin/buildkite-agent start --config ${config.users.users.buildkite.home}/buildkite-agent.cfg";
+    User = "buildkite";
+    RestartSec = 5;
+    Restart = "on-failure";
+    TimeoutSec = 10;
+    # set a long timeout to give buildkite-agent a chance to finish current builds
+    TimeoutStopSec = "2 min";
+    KillMode = "mixed";
+  };
+
+  users.users.robinbb = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    extraGroups = [ "wheel" ];
+    autoSubUidGidRange = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBgTr9+Krb77UzfMIIo0iKEfClzlKl82WZupmY6lTVp"
+    ];
   };
 
   # Hackery required because there's no other way to 'loginctl enable-linger'
   # for the buildkite agent.
   #
   systemd.tmpfiles.rules = [
-    "f /var/lib/systemd/linger/buildkite-agent-gandi-nixos"
+    "f /var/lib/systemd/linger/robinbb"
+    "f /var/lib/systemd/linger/buildkite"
   ];
 
   # Need to figure out how to run this automatically:
   # usermod --add-subuids 100000-165535 --add-subgids 100000-165535 buildkite-agent-gandi-nixos
-  environment.etc = {
-    subuid = {
-      text = "buildkite-agent-gandi-nixos:100000:65536";
-      mode = "0644";
-    };
-    subgid = {
-      text = "buildkite-agent-gandi-nixos:100000:65536";
-      mode = "0644";
-    };
-  };
+#  environment.etc = {
+#    subuid = {
+#      text = "buildkite-agent-gandi-nixos:100000:65536";
+#      mode = "0644";
+#    };
+#    subgid = {
+#      text = "buildkite-agent-gandi-nixos:100000:65536";
+#      mode = "0644";
+#    };
+#  };
 
   boot.isContainer = true;
+
+  virtualisation.podman.enable = true;
 
   environment.systemPackages = with pkgs; [
     vim
     tmux
+    podman-compose
   ];
 
   programs.mosh.enable = true;
