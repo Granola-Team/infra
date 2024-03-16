8ddd7280a5cb4438831d84cae88f082e6fed71c9
Robin Bate Boerop <me@robinbb.com>
2024-03-16 09:09:37 +0000

    Improve settings

---
+++ linode/nixos2-config.nix
@@ -0,0 +1,51 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ 
+    ./nixos-ext4-hardware-config.nix
+  ];
+
+  boot.loader.grub.device = "nodev";
+
+  # networking.hostName = "nixos2"; # Define your hostname.
+
+  environment.systemPackages = with pkgs; [
+    mosh
+    tmux
+    vim
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgqUmPrZwBkOtlDgkft1yVL0YoDKdTr6lWvsoNUP6yA"
+  ];
+  users.users.granola = {
+    isNormalUser = true;
+    createHome = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgqUmPrZwBkOtlDgkft1yVL0YoDKdTr6lWvsoNUP6yA"
+    ];
+  };
+  users.users.robinbb = {
+    isNormalUser = true;
+    createHome = true;
+    extraGroups = [ "wheel" ];  # Enable ‘sudo’.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgqUmPrZwBkOtlDgkft1yVL0YoDKdTr6lWvsoNUP6yA"
+    ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
+  system.stateVersion = "23.11"; # Did NOT change this!
+}
