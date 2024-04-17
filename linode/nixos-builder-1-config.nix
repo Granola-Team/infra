844d235a796139bd725c33dec4f4e7d111cfb904
Robin Bate Boerop <me@robinbb.com>
2024-04-17 19:49:58 +0000

    Add more users

--- linode/nixos-builder-1-config.nix
+++ linode/nixos-builder-1-config.nix
@@ -139,6 +139,35 @@ in
     ];
     linger = true;
   };
+  users.users.Isaac-DeFrain = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    createHome = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYRazgzvBpVsVr6IIAF4ERsVW3/z8NeE750FeomSjKiMGfqP8LY76tpojjnQDNV6kmC9xFp3wy59FkYPFFCIr+lagotlpkF4aA/RLgKGDMOSaGgibbJrhawzWqxIACqzO24qNIRlDAhgZ8SitCb3d+0Xfuf61h7Q1vqw1KIa5zcxihd6Sk7IrljF3l1fnMXGw7jIZ/2toaJ8wJS0tvgajkMN/MtHXNWzhFO+z+2IFzhJmlDfYYQYwVkToscmK4TVj6ji/H79xaYRoK9DZYxzbwKY8ac7cms2cRm+Nt+UBzHKFSzocjaBVuDfyx55VJi0TB4V8b756F5hek9giTcezx"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjLEdzDUNZxefkwTDm7Q6V+nqLX5/yya7MPxG4Fh12hF9jRq8ywjODNV9PXDkOlyxf0gQK67iS81aX5DzLDtP2T1Q8Irw+XrmutwGEA+cCMFwGfebtesd3CxZd3I47UmWgiD0Ba53JGA2kS0NGBCWj6EUyuMD9a6ZJim8AP4NX8TeB37vtCwpf0WTc6wVUNFx/Ee1xqg/k4bXR2n2rB/FtlkxSwW/EtQvmFqIOa5fMqDDK8LODakjt/WCb3XrMgYcKFXv+nbA+pX1aiNciCtB+lOTYY3lDAL+TvRJpG1sOVKJmzKZCx+CqxDpTcs6P380hsvk+TH/4DllhpMDEOigr"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7X5Z5lnnuEgWtYAeOHLw6RSPC/UqLXCiHKoBSk7gbcov6bfjWe4twinCbPikcNDZ3vAY9U3F/35TbJxG6AVYyBKmoDpFG63TM0eZtqo4BucERQlVCF0vxHT64sRjewX7N7xi6vrl8IHufLErrY4ruOAiPI2cQsgyQvdn0g2y2xekXkxeTRDRX3j1DN9X4EoW66knWB/sZXdShs472j9Xs7UGdW3M+q9Z9YfgRGsY2EUCp1Vt0v10h5/nn8hCzCWlhZ23HZnCcJcDvejePVQQYp2r5JMWnlarlZbhO8vddoGi8BnfcEDgxcaIWc0oQfqxCq4Eo5JdXVvMMPVd8wemvtjQwel88NN7STscZAQ31/eU3gSPwCJMKqP0SVJyJ2VCA1QUXH744xBf3Huf7UkFtw6KFmTbbIJiJFSVIlUNyBTiq3gc6Nh6o/lQt1T10LtsrW4gnyOyZc0irQ3KRyXWJXH/SMrvn+kOd/HyUgxEYyrzUw2tv9Zdp0lbB4s+iksix/ePERoWbdp6OokdXJFwfst64up86pgIFvBb3o/6x/NZn0mBd5wntDnjwigFSVzNUzJhwPVw18NLb40ZWCF3MDNJ8bKsUx8awYJqchsMauHC6wU6S3PsovPgTKx9JqTkwhsSEZaxUM07BRfkElrBj9mfOgGGtSbC3mRPJJ3/3VQ=="
+    ];
+  };
+  users.users.jhult = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    createHome = true;
+    extraGroups = [ "wheel" "docker" ];  # Enable ‘sudo’.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwpp6p5298n5Ffk7i33uAPVLFdYLbDJFAYPz/9xHjHN"
+    ];
+  };
+  users.users.trevorbernard = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    createHome = true;
+    extraGroups = [ "wheel" "docker" ];  # Enable ‘sudo’.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHynx+wu6p1AVG8wbSKCALE+q6tH5e1gxCikrvoY0dJE"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrN7T11bia+XlHEOW7DiWyL8iJitys6RjGM4gZXpFVK"
+    ];
+  };
   users.users.robinbb = {
     isNormalUser = true;
     useDefaultShell = true;
