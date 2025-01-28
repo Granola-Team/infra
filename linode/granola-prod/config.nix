7acd601260f132897e7f9706fa2e5523841b7696
Jonathan Hult <Jonathan@JonathanHult.com>
2025-01-27 20:03:35 -0600

    Add Nathan to `granola-prod`

--- linode/granola-prod/config.nix
+++ linode/granola-prod/config.nix
@@ -178,6 +178,16 @@ in
     ];
   };
 
+  users.users.n1tranquilla = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    createHome = true;
+    extraGroups = [ "wheel" "docker" "keys" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlvbrQLfRCNxi9eprfKiJeT/y2cJ1ix4jwR4RhDqFHK"
+    ];
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.trusted-users = [ "root" "@wheel" ];
 
