31cce3089d1d21c47449488e4455e6c33ba1939a
Robin Bate Boerop <me@robinbb.com>
2025-04-23 00:06:59 -0700

    Add support for minasearch-prod-blue

---
+++ linode/minasearch-prod/buildkite.nix
@@ -0,0 +1,86 @@
+{ pkgs, ... }:
+let
+  buildkitePreBootstrap = pkgs.writeScript "buildkite-pre-bootstrap" ''
+    #! /bin/sh
+    set -e
+    # For debugging:
+    if [ -z $BUILDKITE_ENV_FILE ]; then
+      echo "No BUILDKITE_ENV_FILE variable set. Env:"
+      env
+    fi
+  '';
+
+  buildkiteLaunch = pkgs.writeScript "buildkite-agent-launch" ''
+    #!/bin/sh
+    set -eu
+    export BUILDKITE_AGENT_TOKEN="$(cat /run/keys/buildkite-agent-token)"
+    buildkite-agent start --config "$HOME"/buildkite-agent.cfg
+  '';
+
+  secretsImportScript = pkgs.writeScript "secrets-import" ''
+    #! /bin/sh
+    set -eu
+    LINODE_OBJ_ACCESS_KEY="$(cat /run/keys/linode-access-key)"
+    export LINODE_OBJ_ACCESS_KEY
+    LINODE_OBJ_SECRET_KEY="$(cat /run/keys/linode-secret-key)"
+    export LINODE_OBJ_SECRET_KEY
+  '';
+
+  hooksPath = pkgs.runCommandLocal "buildkite-agent-hooks" {} ''
+    mkdir $out
+
+    ln -s ${buildkitePreBootstrap} $out/pre-bootstrap
+
+    cat > $out/pre-checkout << EOF
+    BUILDKITE_GIT_CLEAN_FLAGS='-ffdx'
+    export BUILDKITE_GIT_CLEAN_FLAGS
+    EOF
+
+    ln -s ${secretsImportScript} $out/environment
+  '';
+
+in
+{
+  users.users.bk = {
+    isNormalUser = true;
+    createHome = true;
+    extraGroups = [ "keys" ];
+    shell = pkgs.bash;
+    packages = [ pkgs.buildkite-agent ];
+  };
+
+  systemd.services.buildkite-agent = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    environment = {
+      HOME = "/home/bk";
+    };
+    path = [
+      pkgs.buildkite-agent
+      "/etc/profiles/per-user/bk"
+      "/run/current-system/sw"
+    ];
+    preStart = ''
+      set -u
+      cat > "$HOME/buildkite-agent.cfg" <<EOF
+      name="minasearch-prod-blue"
+      spawn=1
+      priority=1
+      tags="minasearch-prod=true,nix=true,os-kernel=linux,os-family=nixos,os-variant=nixos"
+      build-path="$HOME/builds"
+      hooks-path="${hooksPath}"
+      EOF
+    '';
+    serviceConfig = {
+      User = "bk";
+      Group = "keys";
+      ExecStart = buildkiteLaunch;
+      RestartSec = 5;
+      Restart = "on-failure";
+      TimeoutSec = 10;
+      TimeoutStopSec = "2 min";
+      KillMode = "mixed";
+      LimitNOFILE = 123456;
+    };
+  };
+}
---
+++ linode/minasearch-prod/config.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ./hardware.nix
+    ./buildkite.nix
+    ./users.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.tmp.cleanOnBoot = true;
+
+  networking.hostName = "minasearch-prod";
+
+  # For the mina-indexer:
+  #
+  # First, run:
+  # nix-env -iA nixos.cloudflared
+  # cloudflared tunnel login
+  # cloudflared tunnel token --cred-file /root/.cloudflared/tunnel.json nixos-builder-1
+  #
+  services.cloudflared = {
+    enable = false;
+    user = "root";
+    tunnels = {
+      "950210ce-d5a3-477e-b4e0-2b097732110c" = {
+        credentialsFile = "${config.users.users.root.home}/.cloudflared/tunnel.json";
+        default = "http_status:404";
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    cloudflared
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nofile"; type = "-"; value = "1234567"; }
+  ];
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
+  programs.mosh.enable = true;
+  programs.tmux.enable = true;
+  programs.zsh.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
+  system.stateVersion = "23.11"; # Do NOT change this!
+}
