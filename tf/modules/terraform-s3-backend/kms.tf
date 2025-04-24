b1ed91276a1fee7fe8173941837f1a1ace88e34e
Robin Bate Boerop <me@robinbb.com>
2025-04-23 19:20:00 -0700

    Remove unused Terraform stuff

--- tf/modules/terraform-s3-backend/kms.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_kms_key" "state_key" {
-  description         = "This key is used to encrypt the terraform state bucket and dynamodb table"
-  enable_key_rotation = true
-}
-
-resource "aws_kms_alias" "state_key_alias" {
-  name          = "alias/state-key-${var.environment}"
-  target_key_id = aws_kms_key.state_key.key_id
-}
